53f2d491dc5beefeac451b953fec3aa8ee4ef0b6
Kristijan Bartol <kristijan.bartol@gmail.com>
2021-03-05 13:43:45 +0000

    Update docker run script to name the container.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-docker run --rm --gpus all -it -v /home/dbojanic/pose/learnable-triangulation-pytorch/:/learnable-triangulation/ learnable-triangulation
+docker run --rm --gpus all --name kbartol-triangulation -it -v /home/dbojanic/pose/learnable-triangulation-pytorch/:/learnable-triangulation/ learnable-triangulation
