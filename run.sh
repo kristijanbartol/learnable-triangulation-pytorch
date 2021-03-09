86273580a26232373a53061c66c0e4bc197206e2
Kristijan Bartol <kristijan.bartol@gmail.com>
2021-03-09 07:43:22 +0000

    Update run.sh after fixing .git.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ "$(whoami)" == "kristijan" ]]; then
-	REPO_DIR=/home/kristijan/phd/pose/learnable-triangulation-pytorch/learnable-triangulation-pytorch/
+	REPO_DIR=/home/kristijan/phd/pose/learnable-triangulation-pytorch/
 else
 	REPO_DIR=/home/dbojanic/pose/learnable-triangulation-pytorch/
 fi
