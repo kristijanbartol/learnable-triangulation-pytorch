f434e7f90de0c394f8921760d26ca47050a1bba7
Kristijan Bartol <kristijan.bartol@gmail.com>
2021-04-28 10:18:46 +0000

    Make original code work.

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [[ "$(whoami)" == "kristijan" ]]; then
+			REPO_DIR=/home/kristijan/phd/pose/learnable-triangulation-pytorch/
+				else
+								REPO_DIR=/home/dbojanic/pose/learnable-triangulation-pytorch/
+fi
+
+echo ${REPO_DIR}
+
+docker run --rm --gpus all --name kbartol-learnable -it \
+	-v ${REPO_DIR}:/learnable-triangulation \
+	-v /media/kristijan/kristijan-hdd-ex1/datasets/human36m/:/data/human36m kbartol-learnable
